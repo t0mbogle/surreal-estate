4e4db3e6c69faf97d5b3fd6963bee4d20a729f86
Thomas <thomas.bogle@outlook.com>
2022-06-20 14:31:32 +0100

    Fixed eslint brackets

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from '../components/App.js'
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
 
-it('renders Surreal Estate heading text', () => {
+it("renders Surreal Estate heading text", () => {
   render(<App />);
   const linkElement = screen.getByText(/surreal estate/i);
   expect(linkElement).toBeInTheDocument();
