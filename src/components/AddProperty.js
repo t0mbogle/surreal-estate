86a2567f0f76774fa9390ff89545fd454550e52e
Thomas <thomas.bogle@outlook.com>
2022-06-22 15:30:43 +0100

    Added some CSS styling to add property page

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -29,93 +29,111 @@ const AddProperty = () => {
   return (
     <div className="add-property">
       Add Property Page
-      <form onSubmit={handleAddProperty}>
-        <label htmlFor="title">
-          Title
-          <input
-            id="title"
-            name="title"
-            placeholder="2 Bedroom Flat"
-            value={fields.title}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label htmlFor="city">
-          City
-          <select
-            id="city"
-            name="city"
-            value={fields.city}
-            onChange={handleFieldChange}
-          >
-            <option value="Manchester">Manchester</option>
-            <option value="Leeds">Leeds</option>
-            <option value="Liverpool">Liverpool</option>
-            <option value="Sheffield">Sheffield</option>
-          </select>
-        </label>
-        <label htmlFor="type">
-          Type
-          <select
-            id="type"
-            name="type"
-            value={fields.type}
-            onChange={handleFieldChange}
-          >
-            <option value="Flat">Flat</option>
-            <option value="Detached">Detached</option>
-            <option value="Semi-Detached">Semi-Detached</option>
-            <option value="Terraced">Terraced</option>
-            <option value="End of Terrace">End of Terrace</option>
-            <option value="Cottage">Cottage</option>
-            <option value="Bungalow">Bungalow</option>
-          </select>
-        </label>
-        <label htmlFor="bedrooms">
-          Bedrooms
-          <input
-            id="bedrooms"
-            name="bedrooms"
-            type="number"
-            value={fields.bedrooms}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label htmlFor="bathrooms">
-          Bathrooms
-          <input
-            id="bathrooms"
-            name="bathrooms"
-            type="number"
-            value={fields.bathrooms}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label htmlFor="email">
-          E-mail
-          <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="hello@email.com"
-            value={fields.email}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label htmlFor="price">
-          Price p/m £
-          <input
-            id="price"
-            name="price"
-            type="number"
-            min="10.00"
-            step="10.00"
-            max="2500"
-            value={fields.price}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <button type="submit" onChange={setFields}>
+      <form onSubmit={handleAddProperty} className="add-property-form">
+        <div className="add-property-label">
+          <label htmlFor="title">
+            Title
+            <input
+              id="title"
+              name="title"
+              placeholder="2 Bedroom Flat"
+              value={fields.title}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="add-property-label">
+          <label htmlFor="city">
+            City
+            <select
+              id="city"
+              name="city"
+              value={fields.city}
+              onChange={handleFieldChange}
+            >
+              <option value="Manchester">Manchester</option>
+              <option value="Leeds">Leeds</option>
+              <option value="Liverpool">Liverpool</option>
+              <option value="Sheffield">Sheffield</option>
+            </select>
+          </label>
+        </div>
+        <div className="add-property-label">
+          <label htmlFor="type">
+            Type
+            <select
+              id="type"
+              name="type"
+              value={fields.type}
+              onChange={handleFieldChange}
+            >
+              <option value="Flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detached">Semi-Detached</option>
+              <option value="Terraced">Terraced</option>
+              <option value="End of Terrace">End of Terrace</option>
+              <option value="Cottage">Cottage</option>
+              <option value="Bungalow">Bungalow</option>
+            </select>
+          </label>
+        </div>
+        <div className="add-property-label">
+          <label htmlFor="bedrooms">
+            Bedrooms
+            <input
+              id="bedrooms"
+              name="bedrooms"
+              type="number"
+              value={fields.bedrooms}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="add-property-label">
+          <label htmlFor="bathrooms">
+            Bathrooms
+            <input
+              id="bathrooms"
+              name="bathrooms"
+              type="number"
+              value={fields.bathrooms}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="add-property-label">
+          <label htmlFor="email">
+            E-mail
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="hello@email.com"
+              value={fields.email}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="add-property-label">
+          <label htmlFor="price">
+            Price p/m £
+            <input
+              id="price"
+              name="price"
+              type="number"
+              min="10.00"
+              step="10.00"
+              max="2500"
+              value={fields.price}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <button
+          type="submit"
+          onChange={setFields}
+          className="add-property-button"
+        >
           Add
         </button>
       </form>
