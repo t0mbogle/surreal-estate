332d7478c7fd7758ab4348cad0d181a1d6d0ab36
Thomas <thomas.bogle@outlook.com>
2022-06-25 23:20:47 +0100

    Snapshot tests added for alert message

--- src/__tests__/components/Alert.test.js
+++ src/__tests__/components/Alert.test.js
@@ -3,20 +3,26 @@ import { render } from "@testing-library/react";
 import Alert from "../../components/Alert";
 
 describe("Alert", () => {
-  it("displays an error message", () => {
-    const { getByText } = render(<Alert message="Error!" />);
-
-    expect(getByText(/Error/).textContent).toBe("Error!");
-  });
-  it("displays a success message", () => {
-    const { getByText } = render(<Alert message="Success!" success />);
-
-    expect(getByText(/Success/).textContent).toBe("Success!");
-  });
   it("does not render an error/success message if message prop is empty", () => {
     const { asFragment } = render(<Alert message="" />);
     const alert = asFragment();
 
     expect(alert).toMatchSnapshot();
+  });
+
+  it("displays an error message", () => {
+    const { getByText, asFragment } = render(<Alert message="Error!" />);
+
+    expect(getByText(/Error/).textContent).toBe("Error!");
+    expect(asFragment).toMatchSnapshot();
+  });
+
+  it("displays a success message", () => {
+    const { getByText, asFragment } = render(
+      <Alert message="Success!" success />
+    );
+
+    expect(getByText(/Success/).textContent).toBe("Success!");
+    expect(asFragment).toMatchSnapshot();
   });
 });
