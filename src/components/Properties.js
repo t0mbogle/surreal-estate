3fa0240de33c5b9c65d0eacdfb939c417736d763
Thomas <thomas.bogle@outlook.com>
2022-06-27 21:12:56 +0100

    Properties functionality and styling added

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import PropertyCard from "./PropertyCard";
+import Alert from "./Alert";
 import "../styles/properties.css";
 
 const Properties = () => {
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState({ message: "" });
+
+  useEffect(() => {
+    axios
+      .get("https://surreal-api.herokuapp.com/api/v1/PropertyListing")
+      .then((response) => {
+        setProperties(response.data);
+        setAlert({ message: "Success" });
+        // const { title, type, city, bathrooms, bedrooms, price, email } = getPropertyListings;
+        console.log(response);
+      })
+      .catch((error) => {
+        setAlert({ message: "Error, data could not be retrieved" });
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
       <div>Properties Page</div>
       <div className="property-listings">
-        <PropertyCard />
+        {properties.map((property) => (
+          <div key={property._id} className="item">
+            <PropertyCard {...property} />
+          </div>
+        ))}
       </div>
+      <Alert message={alert.message} />
     </>
   );
 };
