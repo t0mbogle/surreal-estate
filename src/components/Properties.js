b545e32466f56bd46d01efd3ffaacf597360b09a
Thomas <thomas.bogle@outlook.com>
2022-06-29 23:19:28 +0100

    Sidebar functionality added

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import PropertyCard from "./PropertyCard";
 import Alert from "./Alert";
+import Sidebar from "./Sidebar";
 import "../styles/properties.css";
 
 const Properties = () => {
@@ -13,7 +15,7 @@ const Properties = () => {
       .get("https://surreal-api.herokuapp.com/api/v1/PropertyListing")
       .then((response) => {
         setProperties(response.data);
-        setAlert({ message: "Success" });
+        setAlert({ message: "Properties successfully loaded" });
         // const { title, type, city, bathrooms, bedrooms, price, email } = getPropertyListings;
         console.log(response);
       })
@@ -23,10 +25,18 @@ const Properties = () => {
       });
   }, []);
 
+  const { search } = useLocation();
+  useEffect(() => {
+    axios
+      .get(`https://surreal-api.herokuapp.com/api/v1/PropertyListing${search}`)
+      .then((response) => setProperties(response.data))
+      .catch((error) => console.error(error));
+  }, [search]);
+
   return (
     <>
       <Alert message={alert.message} />
-      <div>Properties Page</div>
+      <Sidebar />
       <div className="property-listings">
         {properties.map((property) => (
           <div key={property._id} className="item">
