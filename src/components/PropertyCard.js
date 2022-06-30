6f9df02569905a8af160a18385e57642ed70b448
Thomas <thomas.bogle@outlook.com>
2022-06-30 09:44:33 +0100

    some Sidebar CSS styling

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,12 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-  FaFortAwesome,
-  FaBed,
-  FaBath,
-  FaPoundSign,
-  FaEnvelope,
-} from "react-icons/fa";
+import { FaFortAwesome, FaBed, FaBath, FaEnvelope } from "react-icons/fa";
 import "../styles/property-card.css";
 
 const PropertyCard = ({
@@ -29,9 +23,13 @@ const PropertyCard = ({
       <div className="property-card">
         <b>Property Listing</b> <FaFortAwesome />
       </div>
-      <div className="property-card_title">{title}</div>
+      <div className="property-card_title">
+        <b>{title}</b>
+      </div>
       <div className="property-card_typeCity">
-        {type} - {city}
+        <i>
+          {type} - {city}
+        </i>
       </div>
       <div className="property-card_bathroom">
         <FaBath />
@@ -41,10 +39,7 @@ const PropertyCard = ({
         <FaBed />
         {bedrooms}
       </div>
-      <div className="property-card_price">
-        <FaPoundSign />
-        {newPrice}
-      </div>
+      <div className="property-card_price">{newPrice}</div>
       <div className="property-card_email">
         <a href={`mailto:${email}`}>
           <FaEnvelope /> Contact
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/sidebar.css";
 
 const Sidebar = () => {
   return (
-    <div>
-      <ul>
-        <li>
+    <div className="sidebar">
+      <div className="sidebar-title">Filter results by city:</div>
+      <ul className="sidebar-list">
+        <li className="sidebar-list_city">
           <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
         </li>
-        <li>
+        <li className="sidebar-list_city">
           <Link to={`/?query={"city": "Leeds"}`}>Leeds</Link>
         </li>
-        <li>
+        <li className="sidebar-list_city">
           <Link to={`/?query={"city": "Liverpool"}`}>Liverpool</Link>
         </li>
-        <li>
+        <li className="sidebar-list_city">
           <Link to={`/?query={"city": "Sheffield"}`}>Sheffield</Link>
         </li>
       </ul>
     </div>
   );
 };
+// Add clear feature to remove filtering
 
 export default Sidebar;
